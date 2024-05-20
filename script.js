a7130c1981fdbef7803d4b3130c48e30d52407b9
imranpollob <polboy777@gmail.com>
2024-05-20 00:36:15 -0400

    replace the css with tailwind

---
+++ script.js
@@ -0,0 +1,46 @@
+function setBackgroundColor(tailwindClass) {
+  const body = document.body;
+  // Remove any previously added background color classes
+  body.classList.remove(
+    "bg-black",
+    "bg-gray-900",
+    "bg-gray-800",
+    "bg-gray-700",
+    "bg-gray-600",
+    "bg-gray-500",
+    "bg-gray-400",
+    "bg-gray-300"
+  );
+  // Add the new background color class
+  body.classList.add(tailwindClass);
+}
+
+function updateClock() {
+  const now = new Date();
+  let hours = now.getHours();
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const period = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12; // Convert hours to 12-hour format
+  const clock = document.getElementById("clock");
+  clock.textContent = `${hours}:${minutes}:${seconds} ${period}`;
+}
+
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+updateClock();
+
+document.getElementById("fullscreen-toggle").addEventListener("click", () => {
+  const icon = document.querySelector("#fullscreen-toggle i");
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+    icon.classList.remove("fa-expand");
+    icon.classList.add("fa-compress");
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+    icon.classList.remove("fa-compress");
+    icon.classList.add("fa-expand");
+  }
+});
+
